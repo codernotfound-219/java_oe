add9b56fe79d0d0716aff2a8040ff91a8eb3f778
codernotfound-219 <aswatt219@icloud.com>
2026-03-02 09:01:01 +0530

    update: change var

--- Code/src/mythreads/oddeve/PrinterThread.java
+++ Code/src/mythreads/oddeve/PrinterThread.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 public class PrinterThread extends Thread {
   private int id;
+  private boolean change;
   private final int[] args;
 
   public PrinterThread(int id, int start, int end) {
     this.id = id;
+    change = false;
     args = new int[2];
     args[0] = start;
     args[1] = end;
@@ -29,8 +31,15 @@ public class PrinterThread extends Thread {
     }
   }
 
+  public void getChangeStatus() {
+    System.out.println("Thread :" +id + " : status = " + change);
+  }
+
   @Override
   public void run() {
+    args[1] = 1001;
+    change = true;
+
     if (id == 1) {
       printOddNumbers();
     }
@@ -38,5 +47,6 @@ public class PrinterThread extends Thread {
     if (id == 2) {
       printEvenNumbers();
     }
+    getChangeStatus();
   }
 }
